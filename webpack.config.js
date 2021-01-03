98ac97276f87809745dbc2172a382aa13c879a39
Kai <1114366961@qq.com>
2021-01-03 11:36:56 +0800

    用webpack生成HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      title: 'XDML-写代码啦',
+      template: 'src/assets/index.html'
+    }
+  )]
 };
+
