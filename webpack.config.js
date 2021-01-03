733c0ee35c1af4f9f1456d3da54c96e28cc14a60
Kai <1114366961@qq.com>
2021-01-03 12:12:59 +0800

    4添加 webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -13,6 +19,14 @@ module.exports = {
       title: 'XDML-写代码啦',
       template: 'src/assets/index.html'
     }
-  )]
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
 
